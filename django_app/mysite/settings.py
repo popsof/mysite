de2eab9e31967f0f94ea18df427d67cb409afb87
popsof <popsof@gmail.com>
2016-10-25 16:39:07 +0900

    Add model and make migrations

--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
     'blog',
     'member',
     'video',
+    'photo',
 
     'django.contrib.humanize',
 
--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -8,7 +8,7 @@ class Album(models.Model):
     desc = models.CharField(max_length=80, blank=True)
 
 
-class Photo(modes.Model):
+class Photo(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
     album = models.ForeignKey(Album)
     title = models.CharField(max_length=30)
