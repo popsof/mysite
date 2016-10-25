256dc459d5136ddb8d42d92aaab409e2f2189594
popsof <popsof@gmail.com>
2016-10-25 17:28:25 +0900

    Update photo models.py, add like_users, dislike_users

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -15,4 +15,21 @@ class Photo(models.Model):
     desc = models.CharField(max_length=80, blank=True)
     img = models.ImageField(upload_to='photo')
 
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                through='PhotoLike', related_name='photo_set_like_users')
+    dislike_users = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                through='PhotoDislike', related_name='photo_set_dislike_users')
+
+
+class PhotoLike(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+
+class PhotoDislike(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    create_date = models.DateTimeField(auto_now_add=True)
+
 
