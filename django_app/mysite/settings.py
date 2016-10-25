33e85d880ca1367506a9caa54953db3294a52960
popsof <popsof@gmail.com>
2016-10-25 15:44:43 +0900

    Change configurations at setting.py

--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -11,12 +11,19 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 STATIC_DIR = os.path.join( BASE_DIR, "static" )
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+
+# 설정파일 폴더
+CONF_DIR = os.path.join(BASE_DIR, ".conf")
+
+config_file = open(os.path.join(CONF_DIR, 'settings_debug.json'))
+config = json.loads(config_file.read())
+config_file.close()
 
 STATICFILES_DIRS = [
     STATIC_DIR,
